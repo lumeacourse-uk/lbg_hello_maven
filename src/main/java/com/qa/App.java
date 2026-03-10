7f6fa170def840ae90d62622671d7d5cf1195347
Lu <lumeacourse@gmail.com>
2026-03-10 15:30:50 +0000

    EDIT: HELLO everyone 1.2

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -6,18 +6,25 @@ package com.qa;
  */
 public class App 
 {
+       
     public static void main( String[] args )
     {
+        
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Everyone!!!"));
-        System.out.println(sayGoodbye());
+        System.out.println(sayHelloToSomeone("Line to show that Ive updated the code"));
+                System.out.println(sayGoodbye());
     }
 
     public static String sayHello(){
+        return "Hello World!";
+    }
+
+   public static String count(){
         return "Hello World!";
     }
 
@@ -26,6 +33,7 @@ public class App
     }
 
     public static String sayHelloToSomeone(String name){
-        return "Hello " + name ;
+
+        return "Hello " + name;
     }
 }
